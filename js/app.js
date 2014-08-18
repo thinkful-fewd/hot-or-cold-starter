48495d5d6493d07a69293573d7ce4ee38b45d703
Matt Jennings <matt@mattjennings.net>
2014-08-17 19:24:34 -0700

    random number variable not working so trying to get feedback ID error message css working

--- js/app.js
+++ js/app.js
@@ -6,8 +6,6 @@ $(function(){
 	// Generate random number & validate guesses with min being 1 and max being 100
 	function generateValidateNumber() {
 
-		// Random number variable
-		var randomNum;
 		// Get user number
 		var userNum = +$("#userGuess").val();
 		// Error or Success Message Feedback
@@ -35,46 +33,51 @@ $(function(){
 				$("#feedback").html("Make your Guess!");
 				$("#userGuess").val("");
 				$("#count").html("0");
-				randomNum = "";
+
+				// Reset & generate random number from 1-100
+				var randomNum = "";
+				randomNum = Math.floor(Math.random() * (100) + 1)
+
+					// On submit error, hot, cold & success messages of user number guesses
+					$("#guessButton").on("click", function(event) {
+						event.preventDefault();
+
+						// Show answer
+						$("h1").html("ANSWER: " + randomNum);
+
+						// Error for not using a number or whole numbers not 1-100 inclusive
+	//				if(isNaN(userNum) || userNum != Math.floor(userNum) || userNum > 100 || userNum < 1) {
+	//					feedback.html("Sorry, you need to enter a whole number from 1 - 100 inclusive. Please try again.");
+	//				}
+						// Hot or cold error messages
+						if(Math.abs(randomNum - userNum) > 30) {
+							feedback.html("You're very cold");
+						}
+						else if(Math.abs(randomNum - userNum) < 30 && Math.abs(randomNum - userNum) > 15) {
+							feedback.html("You're getting warmer");
+						}
+						else if(Math.abs(randomNum - userNum) < 15 && Math.abs(randomNum - userNum) > 5) {
+							feedback.html("You're getting hotter");
+						}
+						else if(Math.abs(randomNum - userNum) < 5 && Math.abs(randomNum - userNum) > 0) {
+							feedback.html("You're very hot!");
+						}
+						else if(randomNum === userNum) {
+							feedback.html("You are correct!");
+						}
+
+
+				});
+
+
+
+
 			});
 		}
 		newGame();
 
 		function errorValidation() {
 
-			// On submit error, hot, cold & success messages of user number guesses
-			$("#guessButton").on("click", function(event) {
-				event.preventDefault();
-
-				// Generate random number from 1-100
-				randomNum = Math.floor(Math.random() * (100) + 1)
-
-				// Show answer
-				$("h1").html("ANSWER: " + randomNum);
-
-				// Error for not using a number or whole numbers not 1-100 inclusive
-//				if(isNaN(userNum) || userNum != Math.floor(userNum) || userNum > 100 || userNum < 1) {
-//					feedback.html("Sorry, you need to enter a whole number from 1 - 100 inclusive. Please try again.");
-//				}
-				// Hot or cold error messages
-				if(Math.abs(randomNum - userNum) > 30) {
-					feedback.html("You're very cold");
-				}
-				else if(Math.abs(randomNum - userNum) < 30 && Math.abs(randomNum - userNum) > 15) {
-					feedback.html("You're getting warmer");
-				}
-				else if(Math.abs(randomNum - userNum) < 15 && Math.abs(randomNum - userNum) > 5) {
-					feedback.html("You're getting hotter");
-				}
-				else if(Math.abs(randomNum - userNum) < 5 && Math.abs(randomNum - userNum) > 0) {
-					feedback.html("You're very hot!");
-				}
-				else if(randomNum === userNum) {
-					feedback.html("You are correct!");
-				}
-
-
-			});
 
 		}
 		errorValidation();
