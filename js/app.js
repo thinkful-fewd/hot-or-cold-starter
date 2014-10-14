6d06bfb67db63c4ff69d86497e9f971561d10395
LEADR <ian.fennie@gmail.com>
2014-10-13 19:48:35 -0700

    Moving away from use of "this".
    Previous technique was preventing progress.

--- js/app.js
+++ js/app.js
@@ -1,4 +1,3 @@
-
 $(document).ready(function(){
 	function debug(input) {
 		console.log('Debug', input);
@@ -20,9 +19,9 @@ $(document).ready(function(){
 		this.max = 100;
 
 		// Generate random number w/ named function
-		this.randomNum = Math.floor(Math.random() * (this.max - this.min + 1) + this.min);
+		var randomNum = Math.floor(Math.random() * (this.max - this.min + 1) + this.min);
 
-		console.log("Random Number", this.randomNum);
+		console.log("Random Number", randomNum);
 
 		// Clear h2#feedback
 
@@ -32,7 +31,7 @@ $(document).ready(function(){
 
 		var
 			guess,
-			feedback;
+			hint;
 
 		function checkInput(input) {
 
@@ -41,46 +40,58 @@ $(document).ready(function(){
 				alert("That's not a number! Please enter a number between 1 & 100.");
 			} else if ((input % 1) !== 0) {
 				alert("That's a decimal! Please enter a whole number integer between 1 & 100.");
+				return;
 			} else if (input < 0) {
 				alert("That's a negative number! Please enter a positive number between 1 & 100.");
+				return;
+			} else if (input > 100) {
+				alert("That number is greater than 100! Please enter a number between 1 & 100.");
+				return;
+			} else if (input < 1) {
+				alert('That number is less than 1! Please enter a number between 1 & 100.');
+				return;
+			} else {
+				input = +input;
 			}
-
 		}
-
 
 		// Name function that accepts user guess and delivers feedback
-		function makeGuess(guess) {
-
-			// Absolute value ranges for feedback
-			if ((guess - this.randomNum) >= 50) {
-				feedback = "Ice cold!";
-			} else if (30 >= (guess - this.randomNum) > 50) {
-				feedback = "Cold...";
-			} else if (20 >= (guess - this.randomNum) > 30) {
-				feedback = "Warm...";
-			} else if (10 >= (guess - this.randomNum) > 20) {
-				feedback = "Hot!";
-			} else if (1 >= (guess - this.randomNum) > 10) {
-				feedback = "Very hot!!";
-			}
-
-
-			// return this.randomNum == guess;
+		function guessDiff(guess) {
+			return Math.abs(guess - randomNum);
 		}
 
-		$('#guessForm').on('submit', function() {
-			event.preventDefault();
-
+		function submitEvent() {
 			guess = $('#userGuess').val();
 
 			checkInput(guess);
 
+			var diff = guessDiff(guess);
+
+			// Absolute value ranges for feedback
+			if (diff >= 50) {
+				hint = "Ice cold!";
+			} else if (30 >= diff > 50) {
+				hint = "Cold...";
+			} else if (20 >= diff > 30) {
+				hint = "Warm...";
+			} else if (10 >= diff > 20) {
+				hint = "Hot!";
+			} else if (1 >= diff > 10) {
+				hint = "Very hot!!";
+			} else {
+				hint = 'undefined';
+			}
+
 			// Feedback appears in h2#feedback
-			$('#feedback').text(feedback);
-		});
+			$('#feedback').text(hint);
 
 			// Guess count feedback to appear in span#count
+			var count = $('#count').val();
+			count++;
 
+		}
+
+		$('#guessForm').on('submit', submitEvent);
 
 			// Supply users with list of guesses in ul#guessList
 
