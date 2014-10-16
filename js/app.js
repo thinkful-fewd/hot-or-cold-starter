c1291944441b16a7b8f654102a21287f624e2320
LEADR <ian.fennie@gmail.com>
2014-10-16 00:00:54 -0700

    More refactoring

--- js/app.js
+++ js/app.js
@@ -16,7 +16,6 @@ $(document).ready(function(){
 
 	// Create newGame function
 	function Game() {
-		// Reset
 		$('#feedback').text('Make your guess!');
 		$('#count').text(0);
 		$('#guessList').children().remove();
@@ -26,62 +25,58 @@ $(document).ready(function(){
 		this.min = 1;
 		this.max = 100;
 		this.randomNum = Math.floor(Math.random() * (this.max - this.min + 1) + this.min);
-		console.log("Random Number", randomNum);
-
 		this.guessArray = [];
 
 		// Name function that accepts user guess and delivers feedback
 		this.checkGuess = function(input) {
-
 			input = +input;
-
-			this.validInput = function() {
-				guessArray.push(input);
-
-				function diff() {
-					return Math.abs(input - randomNum);
-				}
-
-				if ( diff() >= 50) {
-					hint = "Ice cold!";
-				} else if ( (diff() < 50) && (diff() >= 30) ) {
-					hint = "Cold...";
-				} else if ( (diff() < 30) && (diff() >= 20) ) {
-					hint = "Warm...";
-				} else if ( (diff() < 20) && (diff() >= 10) ) {
-					hint = "Hot!";
-				} else if ( (diff() < 10) && (diff() >= 1) ) {
-					hint = "Very hot!!";
-				} else if ( diff() === 0) {
-					hint = 'You guessed it!';
-					$('#guessButton').val('WINNER').attr('disabled', true);
-				}
-
-				$('#feedback').text(hint);
-				$('#count').html(function(i, val) {
-					return +val + 1;
-				});
-				$('#guessList').append("<li class='guess'>"+input+"</li>");
-				$('#userGuess').val('');
-			};
 
 			// Modal window alerts
 			if (isNaN(input)) {
 				$('#nanAlert').fadeIn(400);
+				return;
 			} else if ((input % 1) !== 0) {
 				$('#integerAlert').fadeIn(400);
+				return;
 			} else if (input > 100) {
 				$('#greatAlert').fadeIn(400);
+				return;
 			} else if (input < 1) {
-				debug(input);
 				$('#lessAlert').fadeIn(400);
+				return;
 			} else if (guessArray.every( function(val, index, array) {
 				return input !== val;
 				}) !== true) {
 				$('#repeatAlert').fadeIn(400);
-			} else {
-				validInput();
+				return;
 			}
+
+			function diff() {
+				return Math.abs(input - randomNum);
+			}
+			var hint;
+			if ( diff() >= 50) {
+				hint = "Ice cold!";
+			} else if ( (diff() < 50) && (diff() >= 30) ) {
+				hint = "Cold...";
+			} else if ( (diff() < 30) && (diff() >= 20) ) {
+				hint = "Warm...";
+			} else if ( (diff() < 20) && (diff() >= 10) ) {
+				hint = "Hot!";
+			} else if ( (diff() < 10) && (diff() >= 1) ) {
+				hint = "Very hot!!";
+			} else if ( diff() === 0) {
+				hint = 'You guessed it!';
+				$('#guessButton').val('WINNER').attr('disabled', true);
+			}
+			$('#feedback').text(hint);
+			$('#count').html(function(i, val) {
+				return +val + 1;
+			});
+			$('#guessList').append("<li class='guess'>"+input+"</li>");
+			$('#userGuess').val('');
+
+			guessArray.push(input);
 		};
 
 		$('#guessForm').submit(function(event) {
