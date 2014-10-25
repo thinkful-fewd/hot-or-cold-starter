1492e396a0928456bb883b6b8794ca7ce6ab4899
Scotty Runyan <runyans7@gmail.com>
2014-10-25 11:14:45 -0400

    User can input guess

--- js/app.js
+++ js/app.js
@@ -1,5 +1,7 @@
 $(document).ready(function(){
   /*--- Global Variable Set--- */
+  var numberGuessed = false;
+  var guessCount;
   var randomNumber;
 
   /*--- Sets new game ---*/
@@ -7,8 +9,19 @@ $(document).ready(function(){
 
   /*--- Create a new game --*/
   function newGame(){
+    numberGuessed = false;
+    guessCount = 0;
     randomNumber = generateNumber();
   };
+
+  /*--- Get value on form submit ---*/
+  $('form').submit(function(event) {
+    event.preventDefault();
+    if (!numberGuessed) {
+      userChoice = $('#userGuess').val();
+      console.log('User guesses '+ userChoice);
+    };
+  });
 
   /*--- Generate random number --*/
   function generateNumber(){
