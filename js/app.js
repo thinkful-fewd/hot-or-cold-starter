00618baf5dfa889093b6f9493c7ea7add87b9f59
Chris Hamm <chris.m.hamm@gmail.com>
2015-06-11 23:19:08 -0400

    initial notes

--- js/app.js
+++ js/app.js
@@ -3,14 +3,27 @@ $(document).ready(function(){
 	
 	/*--- Display information modal box ---*/
   	$(".what").click(function(){
-    	$(".overlay").fadeIn(1000);
+    	$(".overlay").fadeIn(500);
 
   	});
 
   	/*--- Hide information modal box ---*/
   	$("a.close").click(function(){
-  		$(".overlay").fadeOut(1000);
+  		$(".overlay").fadeOut(500);
   	});
+
+    //Choose random number 1-100 newGame function
+
+
+    //Accept guesses from input (click and Enter), log answers in ul#guessList, increment guess count
+
+
+    //Define hot/cold ranges & declare win feedback prints to div#feedback
+
+
+    //Handle string and decimal inputs (parseInt) - inside previous function?
+
+    //Reset game
 
 });
 
