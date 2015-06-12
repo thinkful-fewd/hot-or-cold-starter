f4e123bc1e06c58f539cc04de719aeb4d4e0cb20
Chris Hamm <chris.m.hamm@gmail.com>
2015-06-12 14:02:54 -0400

    attemmp

--- js/app.js
+++ js/app.js
@@ -12,11 +12,35 @@ $(document).ready(function(){
   		$(".overlay").fadeOut(500);
   	});
 
+    //prevent page reload
+    $('.guessForm').submit(function(e){
+e.preventDefault();
+        return false;
+    });
     //Choose random number 1-100 newGame function
-
+    var randomNum = Math.floor((Math.random() * 100) + 1);
+    console.log(randomNum);
 
     //Accept guesses from input (click and Enter), log answers in ul#guessList, increment guess count
-
+    $('#guessButton').click(function() {
+        var userGuess = $('#userGuess').val();
+        $('#guessList').prepend('<li>' + userGuess + '</li>');
+        if (userGuess - randomNum == 0) {
+            alert("you won"); //replace
+        }
+       /* else if (userGuess - randomNum >= 5) {
+            alert("Lava Hot!");
+        }
+        else if (userGuess - randomNum >= 10) {
+            alert("Hot!");
+        }
+        else if (userGuess - randomNum >= 25) {
+            alert("Cold :(");
+        }
+        else if (userGuess - randomNum >= 50) {
+            alert("Antarctica, baby!");
+        }*/
+    });
 
     //Define hot/cold ranges & declare win feedback prints to div#feedback
 
@@ -24,6 +48,7 @@ $(document).ready(function(){
     //Handle string and decimal inputs (parseInt) - inside previous function?
 
     //Reset game
+
 
 });
 
