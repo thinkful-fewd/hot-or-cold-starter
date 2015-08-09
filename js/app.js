f7c6d70da8ab3ad1a048d5f1db29d8e9f541d54e
Kate Simmons <kate.a.simmons@gmail.com>
2015-08-09 14:24:54 -0500

    random number successfully generated to the console

--- js/app.js
+++ js/app.js
@@ -1,17 +1,50 @@
 
 $(document).ready(function(){
+
+	//variables
+	var secretNumber = 0;
+
+
+	//random number generator
+	function secretNumberGenerator() {
+		secretNumber = (Math.floor(Math.random()*100));
+		console.log("Secret number = " + secretNumber);
+	}
+
+	secretNumberGenerator();
+
+
+	//get user input
+
+	//validate user input is number between 1 and 100 
+
+	//evaluation user input
+
+	//feedback to user
+
+
+
+
+	//number of attempts
+
+	//list of previously entered numbers
+
+
+
+
+	//new game function
+
+	//start new game
+
 	
 	/*--- Display information modal box ---*/
   	$(".what").click(function(){
     	$(".overlay").fadeIn(1000);
 
   	});
-
   	/*--- Hide information modal box ---*/
   	$("a.close").click(function(){
   		$(".overlay").fadeOut(1000);
   	});
 
 });
-
-
