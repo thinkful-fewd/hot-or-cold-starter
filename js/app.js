ad9641cc41f1531891ccbd67fb668fb12c188e28
mkbach <mybach87@gmail.com>
2015-10-11 23:52:02 -0500

    Add function to New button and conditions for Hot or Cold

--- js/app.js
+++ js/app.js
@@ -13,9 +13,12 @@ $(document).ready(function(){
 	});
 
 
-	var userGuess = $('#userGuess');
-	var	x = getRandomInt(1, 100);
+	var userGuess = $('#userGuess'),
+		x = getRandomInt(1, 100),
+		count = $('#count'),
+		totalCount = 0;
 	userGuess.val('').focus();
+	console.log(x);
 
 	function getRandomInt(min, max) {
 		return Math.floor(Math.random() * (max - min + 1)) + min;
@@ -26,17 +29,38 @@ $(document).ready(function(){
 		return !isNaN(parseFloat(n)) && isFinite(n); 
 	}
 
+	function hotOrCold(y, z) {
+		if (y == z) {
+			$('#feedback').text('Correct!');
+		} else {
+			if (Math.abs(y - z) <= 10) {
+				$('#feedback').text('Very hot!');
+			} else if (Math.abs(y - z) <= 20) {
+				$('#feedback').text('Hot!');
+			} else if (Math.abs(y - z) <= 30) {
+				$('#feedback').text('Warm.');
+			} else if (Math.abs(y - z) <= 50) {
+				$('#feedback').text('Cold.');
+			} else{
+				$('#feedback').text('Ice cold...');
+			}
+		}
+	}
+
 	$('.new').click(function(e) {
 		x = getRandomInt(1, 100);
-		console.log(x);
+		$('#count').text(0);
+		$('#guessList li').remove();
+		$('#feedback').text('Make your Guess!');
 	});
 
 	$('#guessButton').click(function(e) {
 		if (isNumeric(userGuess.val()) && userGuess.val() <= 100 && userGuess.val() >= 1) {
-			console.log(userGuess.val());
 			e.preventDefault();
+			hotOrCold (userGuess.val(), x);
+			totalCount += 1;
+			$('#count').text(totalCount);
 			$('#guessList').append('<li>' + userGuess.val() + '</li>');
-
 		}	else {
 			alert('Please enter a valid number from 1 to 100.');
 		}
