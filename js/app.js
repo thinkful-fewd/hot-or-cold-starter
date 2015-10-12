eac5cacdd11d0078d112015b76f7f077a085e4a7
mkbach <mybach87@gmail.com>
2015-10-11 22:51:37 -0500

    Set input only take in number from 1 to 100

--- js/app.js
+++ js/app.js
@@ -2,16 +2,31 @@
 $(document).ready(function(){
 	
 	/*--- Display information modal box ---*/
-  	$(".what").click(function(){
-    	$(".overlay").fadeIn(1000);
+	$(".what").click(function(){
+		$(".overlay").fadeIn(1000);
 
-  	});
+	});
 
-  	/*--- Hide information modal box ---*/
-  	$("a.close").click(function(){
-  		$(".overlay").fadeOut(1000);
-  	});
+	/*--- Hide information modal box ---*/
+	$("a.close").click(function(){
+		$(".overlay").fadeOut(1000);
+	});
 
+
+	var userGuess = $('#userGuess');
+	userGuess.val('').focus();
+
+	function isNumeric(n) { return !isNaN(parseFloat(n)) && isFinite(n); }
+
+	$('#guessButton').click(function(e) {
+		if (isNumeric(userGuess.val()) && userGuess.val() <= 100 && userGuess.val() >= 1) {
+			console.log(userGuess.val());
+
+		}	else {
+			alert('Please enter a valid number from 1 to 100.');
+		}
+		userGuess.val('').focus();
+	});
 });
 
 
