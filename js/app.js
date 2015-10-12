2c5ab67a38bcf24d8530615b350907c7b64ce715
mkbach <mybach87@gmail.com>
2015-10-11 23:07:51 -0500

    Add guess numbers to the list

--- js/app.js
+++ js/app.js
@@ -14,13 +14,28 @@ $(document).ready(function(){
 
 
 	var userGuess = $('#userGuess');
+	var	x = getRandomInt(1, 100);
 	userGuess.val('').focus();
 
-	function isNumeric(n) { return !isNaN(parseFloat(n)) && isFinite(n); }
+	function getRandomInt(min, max) {
+		return Math.floor(Math.random() * (max - min + 1)) + min;
+	}
+
+
+	function isNumeric(n) { 
+		return !isNaN(parseFloat(n)) && isFinite(n); 
+	}
+
+	$('.new').click(function(e) {
+		x = getRandomInt(1, 100);
+		console.log(x);
+	});
 
 	$('#guessButton').click(function(e) {
 		if (isNumeric(userGuess.val()) && userGuess.val() <= 100 && userGuess.val() >= 1) {
 			console.log(userGuess.val());
+			e.preventDefault();
+			$('#guessList').append('<li>' + userGuess.val() + '</li>');
 
 		}	else {
 			alert('Please enter a valid number from 1 to 100.');
